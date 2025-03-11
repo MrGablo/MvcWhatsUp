c3db8491a679a26bebbc1ac5d5c3be97aacbf9f7
MrGablo <707845@student.inholland.nl>
2025-03-11 11:34:15 +0100

    added a new repository connected to the microsoft azure servers

--- MvcWhatsUp/Repositories/DbUsersRepository.cs
+++ MvcWhatsUp/Repositories/DbUsersRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using MvcWhatsUp.Models;
+using System.Windows.Input;
 namespace MvcWhatsUp.Repositories
 {
     public class DbUsersRepository : IUsersRepository
@@ -69,22 +70,75 @@ namespace MvcWhatsUp.Repositories
 
         User? IUsersRepository.GetbyId(int userId)
         {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = $"SELECT UserId, UserName, MobileNumber, EmailAddress FROM Users WHERE UserId = {userId}";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    User user = ReadUser(reader);
+                    return user;
+                }
+            }
             throw new NotImplementedException();
         }
 
         void IUsersRepository.Add(User user)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = $"INSERT INTO Users(UserName, MobileNumber, EmailAddress)" +
+                    "VALUES (@UserName, @MobileNumber, @EmailAddress)" +
+                    "SELECT SCOPE_IDENTITY();";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@UserName", user.UserName);
+                command.Parameters.AddWithValue("@MobileNumber", user.MobileNumber);
+                command.Parameters.AddWithValue("@EmailAddress", user.EmailAddress);
+
+                command.Connection.Open();
+                user.UserId = Convert.ToInt32(command.ExecuteScalar());
+            }
         }
 
         void IUsersRepository.Update(User user)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "UPDATE Users SET UserName = @UserName, MobileNumber = @MobileNumber," +
+                    "EmailAddress = @EmailAddress WHERE UserId = @Id";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Id", user.UserId);
+                command.Parameters.AddWithValue("@UserName", user.UserName);
+                command.Parameters.AddWithValue("@MobileNumber", user.MobileNumber);
+                command.Parameters.AddWithValue("@EmailAddress", user.EmailAddress);
+
+                command.Connection.Open();
+                int nrOfRowsAffected = command.ExecuteNonQuery();
+                if (nrOfRowsAffected == 0)
+                    throw new Exception("No records updated!");
+            }
         }
 
         void IUsersRepository.Delete(User user)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = $"DELETE FROM Users WHERE UserId = @Id";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@id", user.UserId);
+
+                command.Connection.Open();
+                int nrOfRowsAffected = command.ExecuteNonQuery();
+                if(nrOfRowsAffected == 0)
+                {
+                    throw new Exception("No records deleted!");
+                }
+            }
         }
     }
 }
